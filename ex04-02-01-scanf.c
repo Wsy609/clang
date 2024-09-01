e467e00156c53f14d5e0084898d9ee536bdd3792
WooseungYoon <sojiswoo@naver.com>
2024-09-01 09:56:03 +0900

    clang code

---
+++ ex04-02-01-scanf.c
@@ -0,0 +1,27 @@
+/*  파일명 : ex04-02-01-scanf.c
+
+scanf() 함수
+    데이터 입력에 사용되는 함수
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    char character;
+    int inum;
+    float fnum;
+
+    // scanf("%c", &character);
+    // printf("%c", character);
+
+    // scanf("%d", &inum);
+    // printf("%d", inum);
+
+    // scanf("%d", &inum);
+    // scanf("%f", &fnum);
+
+    printf("%c, %d, %f \n", character, inum, fnum);
+
+    return 0;
+}
